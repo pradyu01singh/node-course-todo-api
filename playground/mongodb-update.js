4408e3d2619229487294031ba0c02d851b874533
pradhuman <pradyu01singh@gmail.com>
2018-05-31 16:45:30 +0530

    add update file

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID('5b0fc9af8ac5fc0cac96e0ef')
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b0fcaf475f88f1fc07a96bb')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // client.close();
+});
